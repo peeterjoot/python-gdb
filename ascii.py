488b61fed45c9ffdc272697069457e6bc4fc0fcd
Peeter Joot <peeter.joot@lzlabs.com>
2017-01-09 15:15:05 -0500

    modified:   ascii.py

--- ascii.py
+++ ascii.py
@@ -12,6 +12,12 @@ import codecs
 #    (gdb) source -v ~/workspace/python-gdb/ascii.py
 #    (gdb) ascii ...
 #
+# This produces an exception on the gdb.Value.string() line.  turns out that there's a built in method for EBCDIC/ASCII conversion
+# in gdb.
+# 
+# (gdb) set target-charset EBCDIC-US
+# (gdb) set target-charset ASCII
+#
 class AsciiCommand (gdb.Command):
     """Ascii converts EBCDIC strings to ASCII."""
 
@@ -21,17 +27,21 @@ class AsciiCommand (gdb.Command):
                                              gdb.COMPLETE_SYMBOL, True)
 
     def invoke (self, arg, from_tty):
-        print 'EBCDIC: ', arg
+        print 'x,EBCDIC: ', arg
 
+        # https://sourceware.org/gdb/onlinedocs/gdb/Values-From-Inferior.html#Values-From-Inferior
         v = gdb.parse_and_eval( arg )
         w = gdb.Value.string( v )
+        #w = gdb.Value.lazy_string( 'cp1047', gdb.Value.fetch_lazy(v) )
         #v = gdb.selected_frame().read_var( arg )
         #top = gdb.newest_frame()
         #v = top.read_var(arg)
-        print 'v: ', arg, ', ', v
-        print 'w: ', arg, ', ', w
-        d = codecs.decode(w, 'cp1047')
+
+        #print 'v: ', arg, ', ', v
+        #print 'w: ', arg, ', ', w
+        print 'converting.'
+        #d = codecs.decode(w, 'cp1047')
         print 'converted.'
-        print 'ASCII: "',d,'"'
+        #print 'ASCII: "',d,'"'
 
 AsciiCommand ()
