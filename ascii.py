74ab440eceefc2fdc70e098b05e570756650ebe8
Peeter Joot <peeter.joot@lzlabs.com>
2017-01-09 15:01:02 -0500

    modified:   ascii.py

--- ascii.py
+++ ascii.py
@@ -7,18 +7,31 @@ import codecs
 #
 # sudo yum install python-pip
 # sudo pip install ebcdic
+# 
+# within gdb:
+#    (gdb) source -v ~/workspace/python-gdb/ascii.py
+#    (gdb) ascii ...
 #
 class AsciiCommand (gdb.Command):
     """Ascii converts EBCDIC strings to ASCII."""
 
     def __init__ (self):
         super (AsciiCommand, self).__init__ ("ascii",
-                                             gdb.COMMAND_SUPPORT,
-                                             gdb.COMPLETE_NONE, True)
+                                             gdb.COMMAND_DATA,
+                                             gdb.COMPLETE_SYMBOL, True)
 
     def invoke (self, arg, from_tty):
         print 'EBCDIC: ', arg
-        d = codecs.decode(arg, 'cp1047')
-        print 'ASCII: ', d
+
+        v = gdb.parse_and_eval( arg )
+        w = gdb.Value.string( v )
+        #v = gdb.selected_frame().read_var( arg )
+        #top = gdb.newest_frame()
+        #v = top.read_var(arg)
+        print 'v: ', arg, ', ', v
+        print 'w: ', arg, ', ', w
+        d = codecs.decode(w, 'cp1047')
+        print 'converted.'
+        print 'ASCII: "',d,'"'
 
 AsciiCommand ()
